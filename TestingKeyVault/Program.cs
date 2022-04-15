c229a33d3884fc059af8ad30949f910d6db750ed
troyf <troyf@TROY-DESKTOP>
2022-04-14 20:42:47 -0400

    Add project files.

---
+++ TestingKeyVault/Program.cs
@@ -0,0 +1,43 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+var keyVaultName = app.Configuration.GetSection("KeyVaultName");
+var kvUri = "https://" + keyVaultName.Value + ".vault.azure.net";
+
+var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+var secret = await client.GetSecretAsync("TestSecret");
+
+
+app.MapGet("/key", () =>
+{
+    return secret.Value.Value;
+})
+.WithName("GetKey");
+
+app.MapGet("/test", () =>
+{
+    return "test";
+})
+.WithName("Test");
+
+app.Run();
