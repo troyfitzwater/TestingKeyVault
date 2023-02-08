c18f51744d5801031adab179f0c946b0e21b59bc
Troy Fitzwater <troyfitzwater@gmail.com>
2023-02-07 22:52:24 -0500

    updating to work with certificates

--- TestingKeyVault/Program.cs
+++ TestingKeyVault/Program.cs
@@ -1,5 +1,6 @@
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
+using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,29 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+var keyVaultName = builder.Configuration.GetValue<string>("KeyVault:Vault");
+var thumbrint = builder.Configuration.GetValue<string>("KeyVault:Thumbprint");
+var tenantId = builder.Configuration.GetValue<string>("TenantId");
+var clientId = builder.Configuration.GetValue<string>("ClientId");
+
+var kvUri = "https://" + keyVaultName + ".vault.azure.net";
+
+// usign cert method
+using var x509Store = new X509Store(StoreLocation.CurrentUser);
+
+x509Store.Open(OpenFlags.ReadOnly);
+
+var x509Certificate = x509Store.Certificates.Find(X509FindType.FindByThumbprint, thumbrint, false)
+    .OfType<X509Certificate2>()
+    .Single();
+
+builder.Configuration.AddAzureKeyVault(new Uri(kvUri), new ClientCertificateCredential(tenantId, clientId, x509Certificate));
+
+
+var secretClient = new SecretClient(new Uri(kvUri), new ClientCertificateCredential(tenantId, clientId, x509Certificate));
+var secret = await secretClient.GetSecretAsync("TestingKeyVault-AppConfig--AppSecret");
 
 var app = builder.Build();
 
@@ -19,27 +43,19 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
-var keyVaultName = app.Configuration.GetSection("KeyVaultName");
-var kvUri = "https://" + keyVaultName.Value + ".vault.azure.net";
-
+// used for managed identity method
 var userAssignedClientId = app.Configuration.GetSection("UserAssignedClientId");
-
-var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = userAssignedClientId.Value}));
-
-var secret = await client.GetSecretAsync("TestSecret");
-
+var managedIdentityClient = new SecretClient(new Uri(kvUri), new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = userAssignedClientId.Value}));
+var managedIdentitySecret = await managedIdentityClient.GetSecretAsync("TestSecret");
 
 app.MapGet("/key", () =>
 {
-    return secret.Value.Value;
-})
-.WithName("GetKey");
+    return secret;
+});
 
-app.MapGet("/test", () =>
+app.MapGet("/managed-identity-key", () =>
 {
-    return "test";
-})
-.WithName("Test");
+    return managedIdentitySecret;
+});
 
 app.Run();
